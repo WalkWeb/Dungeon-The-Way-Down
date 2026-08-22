ab0764fc9e742ecf21f3872f9f9ed788cb3e920c
WalkWeb <av18775@list.ru>
2026-08-22 19:01:50 +0300

    Created basic app structure

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Install and run
+
+docker compose up --build
+
+# Use application
+
+Open in browser http://localhost:5173/
---
+++ src/main.js
@@ -0,0 +1,8 @@
+console.log('Hello from JavaScript!');
+
+const app = document.querySelector('#app');
+
+app.innerHTML = `
+    <h1>Hello from Vite!</h1>
+    <p>JavaScript is working.</p>
+`;
